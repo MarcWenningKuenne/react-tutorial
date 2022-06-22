19f8bc686dd604c2d4387d8f0e61ac48af3f809c
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-22 06:18:14 +0200

    4 First React Practice

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Dies ist der Start eines kostenloses React Tutorials!</h1>
+  /* Surprise! You probably thought you could just forget
+  the line of code you just learned! Nope, not on my watch!
+
+  Try to write that 1-liner of React code again! This time,
+  see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+  <ul>
+    <li>List Item 1</li>
+    <li>List Item 2</li>
+    <li>List Item 3</li>
+  </ul>
 );
