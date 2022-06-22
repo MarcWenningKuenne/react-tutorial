c61dee997966411960a27290103877882f297ca9
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-22 06:24:36 +0200

    Why React? Its declarative

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Challenge: Create your own custom React component!
-// Call it "MainContent", and have it return a simple
-// h1 element that says "I'm learning React!"
-// Afterward, render it
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
 
-function MainContent () {
-  return (
-    <h1>I'm learning React!</h1>
-  )
-}
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const h1 = document.createElement("h1")
+h1.textContent = "some text"
+h1.className = "header"
+document.getElementById("root").appendChild(h1)
 
 
+/*
 root.render(
   <MainContent/>
 );
+*/
