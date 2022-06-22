a33dd545e8a939df1aa611327fd06623b19f6517
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-22 07:24:35 +0200

    16 Parent/Child Component

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import ReactDOM from 'react-dom/client';
 /**
 Challenge: 
 
-Part 1: Create a page of your own using a custom Page component
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
 
-It should return an ordered list with the reasons why you're
-excited to be learning React :)
-
-Render your list to the page
-
-*/
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function GetReasonsWhyIAmExcited() {
+function Navbar() {
+  return(
+    <img src='./images/react-logo.png' width="40"></img>
+  )
+}
+
+function Headline() {
+  return (
+    <h1>Reasons I'm excited to learn React</h1>
+  )
+}
+
+function ReasonsWhyIAmExcited() {
   return (
     <ol>
       <li>Ich möchte gerne programmieren lernen</li>
@@ -25,8 +40,26 @@ function GetReasonsWhyIAmExcited() {
   )
 }
 
+function Footer() {
+  return (
+    <footer>© 2022 Wenning-Künne development. All rights reserved.</footer>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Navbar />
+      <Headline />
+      <ReasonsWhyIAmExcited />
+      <Footer />
+    </div>
+  )
+  
+}
+
 root.render(
-  <GetReasonsWhyIAmExcited />, root
+  <Page />, root
 );
 
 
