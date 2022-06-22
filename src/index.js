2552de0a631857448d9189ccb1a05ef2955846bb
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-22 06:37:02 +0200

    10 Thought experiment

--- src/index.js
+++ src/index.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-/* 
-Challenge: 
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
 
-const navbar = (
-  <nav>
-    <h1>This is my brand</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
+const page = (
+  <div>
+    <nav>
+      <h1>Dies ist die Naviagationsbar</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <div>
+      <h1>Dies ist der Hauptteil der Seite</h1>
+      <h2>Dies ist eine h2 Überschrift</h2>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+    </div>
+  </div>
 )
 
+document.getElementById("root").append(page);
+
+//Der Browser zeigt uns ein Objekt an, da aus dem JSX ein Objekt erzeugt wird. Erst das render erzeugt daraus HTML
+
+/*
 root.render(
-  navbar, root
+  page, root
 );
+*/
 
